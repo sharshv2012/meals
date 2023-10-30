c13c204d4d29c0ece5f653b9bcce2ab1f8c80975
Harsh <sharshvardhan759@gmail.com>
2023-10-30 20:54:44 +0530

    Commit 14 : Added Filters Screen, used SwitchListTile and pushed it from tabs.dart using Navigator when identifier was 'filters'.

---
+++ lib/screens/filters.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class FilterScreen extends StatefulWidget {
+  const FilterScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _FilterScreen();
+  }
+}
+
+class _FilterScreen extends State<FilterScreen> {
+  var _glutenFreeFilterSet = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Your Filters"),
+      ),
+      body: Column(
+        children: [
+          SwitchListTile(
+            value: _glutenFreeFilterSet,
+            onChanged: (isChecked) {
+              setState(() {
+                _glutenFreeFilterSet = isChecked;
+              });
+            },
+            title: Text(
+              "Gluten-Free",
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            subtitle: Text(
+              "Only Includes Gluten Free Meals.",
+              style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 34, right: 22),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data..dart';
 import 'package:meals/models/meal.dart';
 import 'package:meals/screens/categories.dart';
+import 'package:meals/screens/filters.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widget/main_drawer.dart';
 
-class TabsScreen extends StatefulWidget{
-
+class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
   @override
   State<StatefulWidget> createState() {
@@ -14,72 +14,90 @@ class TabsScreen extends StatefulWidget{
   }
 }
 
-class _tabsScreenState extends State<TabsScreen>{
+class _tabsScreenState extends State<TabsScreen> {
   int _selectedPageIndex = 0;
   final List<Meal> _favoriteMeals = [];
 
-  void _showInfoMessage(String message){
+  void _showInfoMessage(String message) {
     ScaffoldMessenger.of(context).clearSnackBars();
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message),),);
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+      ),
+    );
   }
 
-  void _toggleMealFavouriteStatus(Meal meal){
+  void _toggleMealFavouriteStatus(Meal meal) {
     final isExisting = _favoriteMeals.contains(meal);
 
-    if(isExisting == true){
+    if (isExisting == true) {
       setState(() {
         _favoriteMeals.remove(meal);
       });
       _showInfoMessage("Meal is no longer a favorite.");
-     
-    }else{
+    } else {
       setState(() {
         _favoriteMeals.add(meal);
       });
       _showInfoMessage("It's your favorite meal now!");
-      
-      
     }
   }
-  void _selectPage(int index){
+
+  void _selectPage(int index) {
     setState(() {
       _selectedPageIndex = index;
     });
   }
 
-  void _setScreen(String identifier){
-    if(identifier == 'meals'){
+  void _setScreen(String identifier) {
+    if (identifier == 'meals') {
       Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (ctx) => MealsScreen(meals: dummyMeals, title:  "All Meals", onToggleFavourite: _toggleMealFavouriteStatus),
-      ),
-    );
-    }else {
-      Navigator.of(context).pop();
+        MaterialPageRoute(
+          builder: (ctx) => MealsScreen(
+            meals: dummyMeals,
+            title: "All Meals",
+            onToggleFavourite: _toggleMealFavouriteStatus,
+          ),
+        ),
+      );
+    } else if (identifier == "filter") {
+      Navigator.of(context).push(
+        MaterialPageRoute(builder: (ctx) => const FilterScreen()),
+      );
     }
-
   }
+
   @override
   Widget build(BuildContext context) {
-
-
-    Widget activePage = CategoriesScreen(onToggleFavourite: _toggleMealFavouriteStatus,);
+    Widget activePage = CategoriesScreen(
+      onToggleFavourite: _toggleMealFavouriteStatus,
+    );
     var activePageTitle = 'Categories';
-    if(_selectedPageIndex == 1){
-      activePage = MealsScreen(meals: _favoriteMeals, title: '', onToggleFavourite: _toggleMealFavouriteStatus,);
+    if (_selectedPageIndex == 1) {
+      activePage = MealsScreen(
+        meals: _favoriteMeals,
+        title: '',
+        onToggleFavourite: _toggleMealFavouriteStatus,
+      );
       activePageTitle = 'Your Favourites';
     }
     return Scaffold(
       appBar: AppBar(
         title: Text(activePageTitle),
       ),
-      drawer: MainDrawer(onSelectScreen: _setScreen,),
+      drawer: MainDrawer(
+        onSelectScreen: _setScreen,
+      ),
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(
-        onTap: _selectPage,
-        currentIndex: _selectedPageIndex, // it'll control highlighting of current tab.
+        onTap: (index) {
+          _selectPage;
+        }, // flutter gives an index value according to the pressed tab.
+        currentIndex:
+            _selectedPageIndex, // it'll control highlighting of current tab.
         items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.set_meal), label: 'Categories' ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.set_meal), label: 'Categories'),
           BottomNavigationBarItem(icon: Icon(Icons.star), label: "Favourites"),
         ],
       ),
