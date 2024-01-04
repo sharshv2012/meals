265d39b67bef3327ceec2a6b49d40ecb5efe8bdd
Harsh <sharshvardhan759@gmail.com>
2024-01-04 19:11:09 +0530

    Commit 28: Implemented the sliding animation on the gridview in categories screen.

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -8,50 +8,54 @@ import 'package:meals/models/category.dart';
 class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({super.key, required this.availbleMeals});
 
-  
   final List<Meal> availbleMeals;
 
   @override
   State<CategoriesScreen> createState() => _CategoriesScreenState();
 }
 
-class _CategoriesScreenState extends State<CategoriesScreen> with SingleTickerProviderStateMixin{ // used 'with' to merge another class.
-  // if u have multiple animation controller the use TickerProviderStateMixin class.
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  // used 'with' to merge another class.
+  // if u have multiple animation controller the use 
+  //TickerProviderStateMixin class.
   late AnimationController _animationController;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
 
     _animationController = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 3000),
+      duration: const Duration(milliseconds: 2000),
       lowerBound: 0,
       upperBound: 1,
-
     );
+
+    _animationController.forward(); 
+    // run it once , u can use .repeat to repeat the animation.
   }
 
   @override
-  void dispose(){
+  void dispose() {
     _animationController.dispose();
     super.dispose();
-
   }
-  void _selectCategory(BuildContext context, Category category) {
 
-  final filteredMeals = widget.availbleMeals.where( // filtering from the availble meals I got from tabsScreen.
-    (meal) => 
-      meal.categories.contains(category.id),
-    ).toList();
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = widget.availbleMeals
+        .where(
+          // filtering from the availble meals I got from tabsScreen.
+          (meal) => meal.categories.contains(category.id),
+        )
+        .toList();
 
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (context) =>  MealsScreen(
-          meals: filteredMeals, 
+        builder: (context) => MealsScreen(
+          meals: filteredMeals,
           title: category.title,
-          
         ),
       ),
     ); // u can also do Navigator.of(context).push(rout);
@@ -59,8 +63,13 @@ class _CategoriesScreenState extends State<CategoriesScreen> with SingleTickerPr
 
   @override
   Widget build(BuildContext context) {
-    
-      return GridView(
+    return AnimatedBuilder(
+      animation: _animationController,
+
+      //the builder will be rebuild every sec hence to avoid rebuilding of every 
+      //widget, we take them in the child so they won't be build this many times.
+      //the gridview is animating but it is not changing only padding is changing.
+      child: GridView(
         padding: const EdgeInsets.all(18),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
@@ -70,13 +79,19 @@ class _CategoriesScreenState extends State<CategoriesScreen> with SingleTickerPr
         children: [
           for (final category in availableCategories)
             CategoryGridItem(
-              category: category, 
-              onSelectCategory: (){_selectCategory(context, category);},
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
             )
           // u can also map the widgets like :
           //...availableCategories.map((category) => CategoryGridItem(category:category)).toList()
         ],
-      );
-  
+      ),
+      builder: (context, child) => Padding(padding: EdgeInsets.only(
+        top: 100 - _animationController.value * 100, 
+        // @value = 0 padding will be 100 and at value @ 1 padding will be 0.
+      ),child: child),
+    );
   }
 }
